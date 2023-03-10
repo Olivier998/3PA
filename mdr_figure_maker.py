bb497fa52a3b28f974e8943f44d066455468edd1
lefo2801 <lefo2801@usherbrooke.ca>
2023-03-10 13:49:29 -0500

    ajout tags

--- mdr_figure_maker.py
+++ mdr_figure_maker.py
@@ -1,5 +1,6 @@
 from bokeh.layouts import row, column, layout
-from bokeh.models import Div, RangeSlider, Spinner, Slider, Arrow, NormalHead, Rect, Label, WheelZoomTool
+from bokeh.models import Div, RangeSlider, Spinner, Slider, LabelSet, ColumnDataSource, CustomJSFilter, CDSView, \
+    IndexFilter, Line
 from bokeh.plotting import figure
 from bokeh.models.callbacks import CustomJS
 
@@ -18,15 +19,15 @@ import webbrowser
 
 from sklearn.ensemble import RandomForestRegressor
 from tree_structure import VariableTree
+from tree_transcriber import TreeTranscriber
 from utils import get_mdr
 import numpy as np
 
-
 THRESHOLD = 0.5
+METRICS = ['bal_acc', 'sens', 'spec']
 
 
 def generate_mdr(x, y, predicted_prob, pos_class_weight=0.5, tree_depth=2, filename=None):
-
     # Tool header
     tool_header = Div(text=f"<b>MDR (Positive weight = {pos_class_weight})</b>",
                       sizing_mode="stretch_width", align="center",  # height="5vw" ,
@@ -34,13 +35,18 @@ def generate_mdr(x, y, predicted_prob, pos_class_weight=0.5, tree_depth=2, filen
                               "font-size": FontSize.TITLE},
                       stylesheets=[":host {height: 5vw;}"])
 
+    # Sliders
     slider_dr = Slider(start=0, end=100, value=100, step=1, title='Declaration Rate',
                        sizing_mode="stretch_width",
-                       #align='center',
                        styles={"text-align": "center", "font-size": FontSize.SUB_TITLE, "padding": "0.5vw",
                                "width": "75%", "align-self": "center"})
+    slider_depth = Slider(start=0, end=4, value=4, step=1, title='Tree depth',
+                          sizing_mode="stretch_width",
+                          styles={"text-align": "center", "font-size": FontSize.SUB_TITLE, "padding": "0.5vw",
+                                  "width": "75%", "align-self": "center"})
 
     # Section to train misclassification model
+    y_pred = np.array([1 if y_score_i >= THRESHOLD else 0 for y_score_i in predicted_prob])
     error_prob = 1 - np.abs(y - predicted_prob)
     sample_weight = np.array([pos_class_weight if yi == 1 else 1 - pos_class_weight for yi in y])
 
@@ -48,18 +54,32 @@ def generate_mdr(x, y, predicted_prob, pos_class_weight=0.5, tree_depth=2, filen
     ca_rf.fit(x, error_prob, sample_weight=sample_weight)
     ca_rf_values = ca_rf.predict(x)
 
-    ca_profile = VariableTree()
+    ca_profile = VariableTree(max_depth=slider_depth.end)
     ca_profile.fit(x, ca_rf_values)
-    ca_profile_values = ca_profile.predict(x, depth=tree_depth)
 
-    min_values_depth = np.array([min(rf_val, prof_val) for rf_val, prof_val in zip(ca_rf_values, ca_profile_values)])
+    min_cas = {}
+    mdr_depths_dict = {'depth': [], 'values': []}
+    for depth in range(0, slider_depth.end + 1):
+        if depth == 0:
+            min_values_depth = ca_rf_values
+        else:
+            ca_profile_values = ca_profile.predict(x, depth=depth)
+            min_values_depth = np.array([min(rf_val, prof_val) for rf_val, prof_val in
+                                         zip(ca_rf_values, ca_profile_values)])
+        min_cas[depth] = min_values_depth
 
-    y_pred = np.array([1 if y_score_i >= THRESHOLD else 0 for y_score_i in predicted_prob])
-    mdr_depth = get_mdr(y, y_pred, min_values_depth)
+        # Get mdr values for every depth
+        mdr_values = get_mdr(y, y_pred, min_values_depth)
+        # from list of dicts to dict
+        mdr_dict = {k: [dic[k] for dic in mdr_values] for k in mdr_values[0]}
 
-    metrics = list(mdr_depth[0].keys())
-    dr = [mdr_depth[i]['dr'] for i in range(len(mdr_depth))]
-    metrics.remove('dr')
+        # Save values
+        mdr_depths_dict['depth'].append(depth)
+        mdr_depths_dict['values'].append(mdr_dict)
+
+    mdr_depths_data = ColumnDataSource(data=mdr_depths_dict)
+    index_current_data = mdr_depths_dict['depth'].index(slider_depth.value)
+    mdr_current_data = ColumnDataSource(data=mdr_depths_dict['values'][index_current_data])
 
     # color manager
     colors = itertools.cycle(palette)
@@ -67,74 +87,131 @@ def generate_mdr(x, y, predicted_prob, pos_class_weight=0.5, tree_depth=2, filen
     # Plot metrics
     plot_metrics = figure(x_axis_label='Declaration Rate', y_axis_label='Metrics score')
     plot_metrics.axis.axis_label_text_font_style = 'bold'
-    for metric_name, color in zip(metrics, colors):
-        plot_metrics.line(dr, [mdr_depth[i][metric_name] for i in range(len(mdr_depth))],
-                          legend_label=metric_name, line_width=2, color=color)
-
-        #ax.plot(dr, [mdr_depth[i][metric_name] for i in range(len(mdr_depth))], label=metric_name, linewidth=2)
+    for metric_name, color in zip(METRICS, colors):
+        plot_metrics.line(x='dr', y=metric_name,
+                          legend_label=metric_name, line_width=2, color=color, source=mdr_current_data)
 
     # Setup legend
     plot_metrics.legend.click_policy = "hide"
     plot_metrics.right = plot_metrics.legend
 
     # Plot tree
-    plot_tree = figure()#tools=WheelZoomTool())
+    plot_tree = figure(aspect_ratio=1, aspect_scale=1, match_aspect=True)  # tools=WheelZoomTool())
     plot_tree.axis.visible = False
     plot_tree.grid.visible = False
 
-    # First node
-    plot_tree.rect(x=0, y=0, width=20, height=10, fill_color='white', line_color='black', line_width=2)
+    # Get tree nodes
+    tree_getter = TreeTranscriber(tree=ca_profile, dimensions=[20, 10], min_ratio_leafs=0.5)
+    nodes, arrows = tree_getter.render_to_bokeh(x=x, y_true=y, y_prob=predicted_prob, min_cas=min_cas, depth=2)
+    for node in nodes:
+        plot_tree.add_glyph(node)
 
-    # second layer
-    plot_tree.rect(x=-20, y=-15, width=20, height=10, fill_color='white', line_color='black', line_width=2)
-    #plot_tree.rect(x=20, y=-15, width=20, height=10, fill_color='white', line_color='black', line_width=2)
-    rect_1 = Rect(x=20, y=-15, width=20, height=10, fill_color='white', line_color='black', line_width=2)
-    plot_tree.add_glyph(rect_1)
+    for arrow in arrows:
+        plot_tree.add_layout(arrow)
 
-    text = Label(text="AUC = 1.00", x=-29, y=-10-2, tags=[1])  # , text_font_size='2vh'
-    text2 = Label(text="AUC = 0.90", x=-29, y=-10-4, tags=[2])  # , text_font_size='2vh'
-    #text.text_font_size = "100%"
-    cjs = CustomJS(args=dict(labels=[text, text2], label=text, label2=text2, rect=rect_1, figure=plot_tree), code="""
-    var ratio = (6 * rect.width.value / (figure.x_range.end-figure.x_range.start));
-    console.log(labels)
-    console.log(labels.length)
+    # test
+    labels = ColumnDataSource(data=dict(x=[-9, -9, -9],
+                                        y=[3, 1, -1],
+                                        label=[0, 1, 0],
+                                        values=['salut', 'auc = 0.5', 'spec=9']))
+    labels2 = ColumnDataSource(data=dict(x=[-9],
+                                         y=[3],
+                                         label=[0],
+                                         values=['salut']))
+
+    active = list(range(3))
+    filter = IndexFilter(indices=active)
+
+    labelset = LabelSet(x='x', y='y', text='values', source=labels2)
+    plot_tree.add_layout(labelset)  # , view=view
+
+    callback = CustomJS(args=dict(src=labels, lsrc=labels2, filt=filter), code='''
+    filt.indices = [cb_obj.value];//[...Array(cb_obj.value).keys()];
+    src.change.emit();
+
+    var data=src.data;
+    var l=data['label'];
+    var x=data['x'];
+    var y=data['y'];
+    var values=data['values'];
+
+    var ldata=lsrc.data;
+    var a=[cb_obj.value];//[...Array(cb_obj.value).keys()];
+
+    var ll=[];
+    var lv=[];
+    var lx=[];
+    var ly=[];
+    for(var i=0;i<a.length;i++){
+        ll.push(l[a[i]]);
+        lx.push(x[a[i]]);
+        ly.push(y[a[i]]);
+        lv.push(values[a[i]]);
+    }
+
+    ldata['label']=ll;
+    ldata['x']=lx;
+    ldata['y']=ly;
+    ldata['values']=lv;
+    lsrc.change.emit();
+    ''')
+
+    cjs = CustomJS(args=dict(labels=[labelset], width=20, figure=plot_tree), code="""
+    var ratio = (6 * width / (figure.x_range.end-figure.x_range.start));
     for (let i = 0; i < labels.length; i++){
         labels[i].text_font_size = ratio+'vw';
     }
     """)
+    plot_tree.x_range.js_on_change('start', cjs)
 
-    """var ratio = (6 * rect.width.value / (figure.x_range.end-figure.x_range.start));
-    for (let i = 0; i < labels.length; i++){
-        labels[i].text_font_size = ratio+'vw';
-        labels[i].emit();
+    callback2 = CustomJS(args=dict(source=labels), code="""
+        source.change.emit();
+    """)
+    slider_dr.js_on_change('value', callback)
+
+    callback_depth = CustomJS(args=dict(src=mdr_depths_data, curr=mdr_current_data, slider=slider_depth, nodes=nodes),
+                              code='''
+    var depth=slider.value;
+    
+    // Change the MDR section
+    var data=src.data;
+    var depths=data['depth'];
+    var values=data['values'];
+    
+    var depth_index=depths.indexOf(depth);
+    
+    curr.data=values[depth_index];
+    curr.change.emit();
+    
+    // Change the Profile section
+    console.log(nodes[0].tags[1]);
+    for (var i=0; i< nodes.length; i++){
+        if (nodes[i].tags[0] > depth){
+            nodes[i].line_alpha = 0;
+        }
+        else{
+            nodes[i].line_alpha=1;
+        }
     }
     
-    label.text_font_size =  ratio+'vw';
-    label2.text_font_size =  ratio+'vw';
-    
-    label.y = -10 - 2;
-    label2.y = -10 - 4;
-    
-    
-    label.change.emit();
-    label2.change.emit();"""
+    ''')
+    slider_depth.js_on_change('value', callback_depth)
 
-    # console.log(rect.width);
-    # console.log(figure.x_range.start);
-    plot_tree.add_layout(text)
-    plot_tree.add_layout(text2)
 
-    plot_tree.x_range.js_on_change('start', cjs)
-    plot_tree.x_range.js_on_change('end', cjs)
-    #plot_tree.js_on_change('x_range', cjs)
-    #plot_tree.on_change('x_range', cjs)
 
-    # Arrows of second layer
-    plot_tree.add_layout(Arrow(x_start=0, y_start=-5, x_end=-20, y_end=-10, end=None))
-    plot_tree.add_layout(Arrow(x_start=0, y_start=-5, x_end=20, y_end=-10, end=None))
 
-    #tt1 = Div(text="Box1")
-    #tt2 = Div(text="Box2")
+    # js_filter = CustomJSFilter(args=dict(slider=slider_dr), code="""  # , source=labels
+    #    if (slider.value >50){
+    #        desiredElementCount=3;
+    #    }
+    #    else {
+    #    desiredElementCount =2;
+    #    }#
+    #
+    #        return [...Array(desiredElementCount).keys()];
+    #   """)
+
+    #  view = CDSView(filter=[js_filter])
 
     # We set the two box (extracted profiles and MDR curves)
     outline_boxs = row(
@@ -165,7 +242,8 @@ def generate_mdr(x, y, predicted_prob, pos_class_weight=0.5, tree_depth=2, filen
     # create layout
     layout_output = layout([
         tool_header,
-        slider_dr,
+        row(slider_dr,
+        slider_depth, sizing_mode="stretch_width"),
         [outline_boxs],
     ],
         sizing_mode="stretch_both")
@@ -176,8 +254,6 @@ def generate_mdr(x, y, predicted_prob, pos_class_weight=0.5, tree_depth=2, filen
 
     if filename is None or filename == "":
         filename = 'newtest01'
-    #    output_file(filename)
-    #show(layout_output)
     html = file_html(layout_output, CDN, title=filename)
 
     path = os.path.abspath(filename + '.html')
@@ -192,7 +268,9 @@ if __name__ == '__main__':
     # prepare some data
     import pandas as pd
     import numpy as np
-    x = pd.DataFrame([[1,2,3], [4,5,6], [7,8,9], [10, 11, 12]], columns=['a','b','c'])  # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    x = pd.DataFrame([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]],
+                     columns=['a', 'b', 'c'])  # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     y = np.array([1, 1, 0, 0])
     y_pred = np.array([0.98, 0.45, 0.35, 0.02])
     generate_mdr(x, y, y_pred)
--- tree_transcriber.py
+++ tree_transcriber.py
@@ -1,20 +1,85 @@
-from bokeh.models import Rect
+from bokeh.models import Rect, Arrow
 from tree_structure import VariableTree, _Node
+from utils import get_mdr
+import numpy as np
 
 
 class TreeTranscriber:
 
-    def __init__(self, tree: VariableTree):
+    def __init__(self, tree: VariableTree, dimensions=[20, 10], min_ratio_leafs: float = 0.5, THRESHOLD=0.5):
         self.tree = tree
+        self.width = dimensions[0]
+        self.height = dimensions[1]
+        self.min_ratio_leafs = min_ratio_leafs
+        self.THRESHOLD = THRESHOLD
 
-    def render_to_bokeh(self, pos_x=0, pos_y=0, width=20, height=10, min_ratio_leafs: float = 0.5, depth=None, *kwargs):
+    def render_to_bokeh(self, x, y_true, y_prob, min_cas, depth=None, *kwargs):
         if depth is None:
             depth = self.tree.max_depth
 
-        self.__add_node(pos_x=pos_x, pos_y=pos_y, width=width, height=height, min_ratio_leafs=min_ratio_leafs,
-                        remaining_depth=depth, *kwargs)
+        nodes, arrows = self.__add_node(x=x, y_true=y_true, y_prob=y_prob, min_cas=min_cas, curr_node=self.tree.head,
+                                        remaining_depth=depth, curr_depth=0, *kwargs)
 
-    def __add_node(self, pos_x, pos_y, width, height, min_ratio_leafs: float, remaining_depth, *kwargsf ):
-        rect = Rect(x=20, y=-15, width=20, height=10, fill_color='white', line_color='black', line_width=2)
+        return nodes, arrows
 
-        return [rect]
+    def __add_node(self, x, y_true, y_prob, min_cas, remaining_depth, curr_node, curr_depth, pos_x=0, pos_y=0, *kwargs):
+        values_depth_dr = {'depth': [], 'values': []}
+        y_pred = np.array([1 if y_score_i >= self.THRESHOLD else 0 for y_score_i in y_prob])
+        for depth in range(curr_depth, self.tree.max_depth + 1):
+            mdr_values = get_mdr(Y_target=y_true, Y_predicted=y_pred, predicted_accuracies=min_cas[depth])
+            mdr_dict = {'dr': [dic['dr'] for dic in mdr_values]}
+            mdr_dict['metrics'] = [{key: value for key, value in dic.items() if key != 'dr'} for dic in mdr_values]
+
+            values_depth_dr['depth'].append(depth)
+            values_depth_dr['values'].append(mdr_dict)
+
+        rect = Rect(x=pos_x, y=pos_y, width=self.width, height=self.height, fill_color='white', line_color='black',
+                    line_width=2, tags=[curr_depth, values_depth_dr])
+
+        if remaining_depth == 0 or curr_node.c_left is None:
+            return [rect], []
+
+        # left child nodes
+        pos_x_left = pos_x - 1 / 2 * self.width * (1 + self.min_ratio_leafs) * 2 ** (remaining_depth - 1)
+        pos_y_left = pos_y - 2 * self.height
+
+        idx_left = [row_idx for row_idx, row_val in x.iterrows() if row_val[curr_node.feature] <= curr_node.threshold]
+        min_cas_left = {}
+        for depth in min_cas:
+            min_cas_left[depth] = min_cas[depth][idx_left]
+
+        c_left = self.__add_node(x=x.iloc[idx_left],
+                                 y_true=y_true[idx_left],
+                                 y_prob=y_prob[idx_left],
+                                 min_cas=min_cas_left,
+                                 pos_x=pos_x_left,
+                                 pos_y=pos_y_left,
+                                 curr_node=curr_node.c_left,
+                                 remaining_depth=remaining_depth - 1,
+                                 curr_depth=curr_depth + 1)
+
+        arr_left = Arrow(x_start=pos_x, y_start=pos_y - 0.5 * self.height, x_end=pos_x_left,
+                         y_end=pos_y_left + 0.5 * self.height, end=None, line_color="green", line_width=3)
+
+        # right child nodes
+        pos_x_right = pos_x + 1 / 2 * self.width * (1 + self.min_ratio_leafs) * 2 ** (remaining_depth - 1)
+        pos_y_right = pos_y - 2 * self.height
+
+        idx_right = [row_idx for row_idx, row_val in x.iterrows() if row_val[curr_node.feature] > curr_node.threshold]
+        min_cas_right = {}
+        for depth in min_cas:
+            min_cas_right[depth] = min_cas[depth][idx_right]
+
+        c_right = self.__add_node(x=x.iloc[idx_right],
+                                  y_true=y_true[idx_right],
+                                  y_prob=y_prob[idx_right],
+                                  min_cas=min_cas_right,
+                                  pos_x=pos_x_right,
+                                  pos_y=pos_y_right,
+                                  curr_node=curr_node.c_right,
+                                  remaining_depth=remaining_depth - 1,
+                                  curr_depth=curr_depth + 1)
+        arr_right = Arrow(x_start=pos_x, y_start=pos_y - 0.5 * self.height, x_end=pos_x_right,
+                          y_end=pos_y_right + 0.5 * self.height, end=None, line_color="red", line_width=3)
+
+        return [rect, *c_left[0], *c_right[0]], [arr_left, arr_right, *c_left[1], *c_right[1]]
